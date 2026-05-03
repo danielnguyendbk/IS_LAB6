323f97d361af2512d0236e9f7309aaa3f72b12b8
YoungOfVietNam <chaudiensdk5@gmail.com>
2026-05-03 14:28:28 +0700

    feat: apply RandomUnderSampler for BENIGN class

--- src/balance.py
+++ src/balance.py
@@ -19,7 +19,6 @@ def balance_data(X, y):
     rus = RandomUnderSampler(sampling_strategy='not minority', random_state=42)
     X_res, y_res = rus.fit_resample(X, y)
     
-    # 2. Tăng cường các lớp tấn công bằng SMOTE
     # feat: apply SMOTE for minority classes
     smote = SMOTE(sampling_strategy='auto', random_state=42)
     X_final, y_final = smote.fit_resample(X_res, y_res)
